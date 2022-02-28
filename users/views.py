1d7ae218a8f8999457ac527b427a802dce7c0401
angel <angelriera1796@gmail.com>
2022-02-28 13:22:50 -0400

    first commit

---
+++ users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'home.html')
+
+def user_view(request):
+    return render(request, 'home.html')
