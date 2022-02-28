4fd104b6f002d4b6adb5d940b8091afdee12c07f
angel <angelriera1796@gmail.com>
2022-02-28 14:32:32 -0400

    template

--- users/views.py
+++ users/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'users/home.html')
 
-def register(request):
-    return render(request, 'home.html')
+# def register(request):
+#     return render(request, 'home.html')
 
-def login(request):
-    return render(request, 'home.html')
+# def login(request):
+#     return render(request, 'home.html')
 
-def user_view(request):
-    return render(request, 'home.html')
+# def user_view(request):
+#     return render(request, 'home.html')
