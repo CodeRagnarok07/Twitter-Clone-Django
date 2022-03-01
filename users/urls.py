426d74e61bb4f5de304ee142aedb2df038c9dbdb
angel <angelriera1796@gmail.com>
2022-02-28 20:25:11 -0400

    templates allready

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("view_user", views.view_user, name="view_user"),
 ]
--- users/views.py
+++ users/views.py
@@ -5,11 +5,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'users/home.html')
 
-# def register(request):
-#     return render(request, 'home.html')
+def register(request):
+    return render(request, 'users/register.html')
 
-# def login(request):
-#     return render(request, 'home.html')
+def login(request):
+    return render(request, 'users/login.html')
 
-# def user_view(request):
-#     return render(request, 'home.html')
+def view_user(request):
+    return render(request, 'users/view_user.html')
