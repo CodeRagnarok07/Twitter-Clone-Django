c0c83bc32624d1d529459ffd48e4692d52add6dd
angel <angelriera1796@gmail.com>
2022-07-14 16:57:16 -0400

    sadas

--- apps/users/context_processors.py
+++ apps/users/context_processors.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 from .models import User
-=======
-from .models import CustomUser
->>>>>>> 78e8157e87090d617eb91c9d38cd1e4995547a86
 from django.conf import settings
 from django.db.models import Q
 
 def all_profiles(request):
-<<<<<<< HEAD
     all_profiles = User.objects.all()#.exclude(Q(user=request.user.profile))#| Q(id=settings.ANONYMOUS_USER) )
-=======
-    all_profiles = CustomUser.objects.all()#.exclude(Q(user=request.user.profile))#| Q(id=settings.ANONYMOUS_USER) )
->>>>>>> 78e8157e87090d617eb91c9d38cd1e4995547a86
     return {'all_profiles': all_profiles} # NOMBRE DEL OBJETO LLAMABLE DEBEN SER IGUALES
--- apps/users/forms.py
+++ apps/users/forms.py
@@ -2,43 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-<<<<<<< HEAD
 # from django.forms import ModelForm
-=======
-from django.contrib.auth.models import User
-from .models import CustomUser
->>>>>>> 78e8157e87090d617eb91c9d38cd1e4995547a86
 
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
-<<<<<<< HEAD
         model = User
         fields = ['email', 'username', 'name', 'password1', 'password2']
-=======
-        model = CustomUser
-        fields = '__all__'
->>>>>>> 78e8157e87090d617eb91c9d38cd1e4995547a86
 
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
-<<<<<<< HEAD
         model = User
         fields = '__all__'
-=======
-        model = CustomUser
-        fields = '__all__'
-
->>>>>>> 78e8157e87090d617eb91c9d38cd1e4995547a86
 
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
-<<<<<<< HEAD
         model = User
         fields = ('bio','img')#['header','bio', 'img']
-=======
-        model = CustomUser
-        fields = ("bio", "image", "image_header")
->>>>>>> 78e8157e87090d617eb91c9d38cd1e4995547a86
